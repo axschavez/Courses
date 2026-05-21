933d2185e2f505b98b4527ca7e47d2c065b8dec2
Sr. Alexis <alexischavezbarria1@gmail.com>
2026-05-21 08:54:02 -0300

    new: creo migracion de courses y las tablas.

---
+++ database/migrations/2026_05_21_115043_create_courses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('description');
+            $table->decimal('price', 10, 2)->default(0);
+            $table->enum('level', ['beginner', 'intermediate', 'advanced']);
+            $table->enum('status', ['draft', 'published'])->default('draft');
+            $table->string('thumbnail')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
